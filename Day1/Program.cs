ec72d64a80f4113f746df4b6936e1597947c5b64
Mattias Andersson <mattias.andersson@sogeti.se>
2021-12-01 09:41:04 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var depths = System.IO.File.ReadAllLines("Input1.txt");
+            var depthList = new List<int>();
+
+            foreach (var depth in depths)
+            {
+                depthList.Add(int.Parse(depth));
+            }
+
+            Part1(depthList.ToArray());
+            Part2(depthList.ToArray());
+            Console.ReadKey();
+        }
+
+        private static void Part2(int[] depths)
+        {
+            int increments = 0;
+
+            for (int i = 0; i < depths.Length - 3; i++)
+            {
+                int firstSum = depths[i] + depths[i + 1] + depths[i + 2];
+                int secondSum = depths[i + 1] + depths[i + 2] + depths[i + 3];
+
+                if (secondSum > firstSum)
+                    increments++;
+            }
+
+            Console.WriteLine(increments);
+        }
+
+        private static void Part1(int[] depths)
+        {
+            int increments = 0;
+
+            for (int i = 1; i < depths.Length; i++)
+            {
+                int previous = depths[i - 1];
+                int current = depths[i];
+                if (current > previous)
+                    increments++;
+            }
+
+            Console.WriteLine(increments);
+        }
+    }
+}
