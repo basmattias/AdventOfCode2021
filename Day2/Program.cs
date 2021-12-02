47f27ff988f5b17474623aea0242444aa9a27fe2
Mattias Andersson <mattias.andersson@sogeti.se>
2021-12-02 10:53:25 +0100

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = System.IO.File.ReadAllLines(@"Input2.txt");
+
+            var result = Part1(data);
+            Console.WriteLine($"Position 1: horizontal: {result.Item1}, depth: {result.Item2}. Product: {result.Item1 * result.Item2}");
+
+            result = Part2(data);
+            Console.WriteLine($"Position 2: horizontal: {result.Item1}, depth: {result.Item2}. Product: {result.Item1 * result.Item2}");
+
+            Console.ReadKey();
+        }
+
+        private static Tuple<int, int> Part1(string[] data)
+        {
+            var forwards = 0;
+            var ups = 0;
+            var downs = 0;
+
+            foreach (var s in data)
+            {
+                var c = s.Split(' ');
+                var v = int.Parse(c[1]);
+                switch (c[0])
+                {
+                    case "forward":
+                        forwards += v;
+                        break;
+                    case "up":
+                        ups += v;
+                        break;
+                    case "down":
+                        downs += v;
+                        break;
+                    default:
+                        Console.WriteLine("STFU");
+                        throw new ApplicationException();
+                }
+            }
+
+            return Tuple.Create(forwards, downs - ups);
+        }
+
+        private static Tuple<int, int> Part2(string[] data)
+        {
+            var forwards = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var s in data)
+            {
+                var c = s.Split(' ');
+                var v = int.Parse(c[1]);
+    
+                switch (c[0])
+                {
+                    case "forward":
+                        forwards += v;
+                        depth += aim * v;
+                        break;
+                    case "up":
+                        aim -= v;
+                        break;
+                    case "down":
+                        aim += v;
+                        break;
+                    default:
+                        Console.WriteLine("STFU");
+                        throw new ApplicationException();
+                }
+            }
+
+            return Tuple.Create(forwards, depth);
+        }
+    }
+}
---
+++ R-code/Dag2.r
@@ -0,0 +1,8 @@
+
+dag2 <- function()
+{
+  data <- scan("R-code/Input2test.txt", 
+               what = list(Direction = "", Value = 0),
+               sep=" ")
+  
+}
